32a00cd7736133e9068ce1ae3b4fcb202d00ca66
momemtwithace <augustineezekiel763@gmail.com>
2022-11-28 10:32:24 +0100

    Added Models

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground.apps.PlaygroundConfig',
+    'store',
+    'tags',
     # 'debug_toolbar'
 ]
 
