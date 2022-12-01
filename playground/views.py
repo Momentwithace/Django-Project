93e566a199957a2651e3e56c987f790cf4fef77f
momemtwithace <augustineezekiel763@gmail.com>
2022-12-01 02:09:42 +0100

    Used the Q object in django.db.model to change query

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 
 # Create your views here.
 
 def say_hello(request):
-    queryset = Product.objects.filter(title__icontains='coffee')
-
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(price__lt=20))
     return render(request, 'index.html', {
         'name': 'Ace', 'products': list(queryset)
     })
