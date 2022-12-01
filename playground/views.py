6c8b228c6f8ca2d6d32d4eb87483054c3eea6779
momemtwithace <augustineezekiel763@gmail.com>
2022-12-01 01:37:10 +0100

    Called the .objects method on the product model, in other to generate a queryset

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 
 # Create your views here.
 
 def say_hello(request):
+    query_set = Product.objects.all()
+    for obj in query_set:
+        print(obj)
+
     return render(request, 'index.html', {
         'name': 'Ace'
     })
