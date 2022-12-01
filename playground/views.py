7ecd76f070dc42a698fb1b0e100fe3a42bd20627
momemtwithace <augustineezekiel763@gmail.com>
2022-12-01 01:45:48 +0100

    Used try and catch,for exception handling

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 # Create your views here.
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    for obj in query_set:
-        print(obj)
+    try:
+        product = Product.objects.get(pk=0)
+    except ObjectDoesNotExist:
 
-    return render(request, 'index.html', {
-        'name': 'Ace'
-    })
+        return render(request, 'index.html', {
+            'name': 'Ace'
+        })
