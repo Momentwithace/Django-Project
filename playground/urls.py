deb8ff07e8d8a1b9c9129bfff9f0b33acf2074ac
momemtwithace <augustineezekiel763@gmail.com>
2022-12-02 10:51:16 +0100

    Added a new view(hi)

--- playground/urls.py
+++ playground/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('hi/', views.hi)
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 # Create your views here.
 
 def say_hello(request):
-    queryset = Product.objects.all()[4:15]
+    queryset = Product.objects.filter(
+        Q(inventory__lt=10), Q(price__lt=20))
     return render(request, 'index.html', {
-        'name': 'Ace', 'products': list(queryset)
+        'name': 'Ace', 'products': queryset
+    })
+
+
+def hi(request):
+    query_set = Product.objects.order_by("title")
+    return render(request, 'hi.html', {
+        'name': 'Order_by', 'product': list(query_set)
     })
