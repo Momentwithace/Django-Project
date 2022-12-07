de01a6eb55f4ed02d1f11a47228941f736fbb900
momemtwithace <augustineezekiel763@gmail.com>
2022-12-08 00:47:43 +0100

    Registered the Customer Model and Product Model to the admin model in my project

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from . import models
+
+
 # Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+    list_editable = ['price']
+    list_per_page = 10
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_editable = ['membership']
+    list_display = ['first_name', 'last_name', 'membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+
 
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
+
